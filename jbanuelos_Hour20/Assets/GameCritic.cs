80338d5121c1a1600b0bd413414baae61909a76b
Jose B <banuelosj45@gmail.com>
2024-12-08 23:13:50 -0800

    Created Gauntlet Runner

---
+++ jbanuelos_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,18 @@
+/* Game Critic
+
+1) The game is too wasy because the obstacles move very slow even witht the limited view from the camera angle.
+Another reason why the game is too easy is because the obstacles are extremely small. This combined with the slow speed makes it easy avoid.
+
+2) I would make the obstacles vary in speed and size.
+
+3) A change in scenary and more attractive particles would the game pop more.
+
+4) Avoiding obstacles and reaching for extremely hard and risky powerups makes the game fun.
+
+5) The sluggish movements and lack of fianl reward make the game tedious.
+
+10 a) obstacle got spiked added to it  
+
+10 b) power up got a model change with a power up aura and a hourglass
+
+*/
